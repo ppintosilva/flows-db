fa311e7da17252ac7f3164c5ffd9d0ac342cb9b3
Pedro Pinto da Silva <ppintodasilva@gmail.com>
2020-01-07 12:00:22 +0000

    Add aggregate queries

---
+++ scripts/agg_hour_weekday.sql
@@ -0,0 +1,23 @@
+INSERT INTO
+  agg_hour_weekday (o,d,hour,weekday,sum_flow,median_flow,
+                    sum_mean_avspeed,median_mean_avspeed,
+                    sum_sd_avspeed, median_sd_avspeed)
+(
+  SELECT
+      o
+    , d
+    , EXTRACT(hour FROM t)   AS hour
+    , EXTRACT(dow  FROM t)   AS weekday
+    , sum(flow)              AS sum_flow
+    , median(flow)           AS median_flow
+    , sum(mean_avspeed)      AS sum_mean_avspeed
+    , median(mean_avspeed)   AS median_mean_avspeed
+    , sum(sd_avspeed)        AS sum_sd_avspeed
+    , median(sd_avspeed)     AS median_sd_avspeed
+    , sum(median_avspeed)    AS sum_median_avspeed
+    , median(median_avspeed) AS median_median_avspeed
+  FROM od_1hour
+  GROUP BY o,d,weekday,hour
+  ORDER BY o,d,weekday,hour
+)
+;
