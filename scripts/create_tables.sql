7cdb2833d6d1584e25c0d1297fbb245179931f5e
Pedro Pinto da Silva <ppintodasilva@gmail.com>
2020-04-07 17:18:33 +0100

    Refactor pg table names and drop aggregate tables

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,32 +1,29 @@
-CREATE TABLE od_05min
+CREATE TABLE flows05m
 (
   o INTEGER NOT NULL,
   d INTEGER NOT NULL,
   t TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-  flow INTEGER,
-  median_avspeed NUMERIC,
-  mean_avspeed NUMERIC,
-  sd_avspeed NUMERIC,
+  count INTEGER,
+  median_speed NUMERIC,
+  mean_speed NUMERIC,
+  sd_speed NUMERIC,
   PRIMARY KEY(o,d,t)
 );
 
 -- Duplicate the above table for all different aggregation resolutions
 -- NOT NULL constraint on t does not carry on so enforce it
 
-CREATE TABLE od_15min   AS (SELECT * FROM od_05min) WITH NO DATA;
-ALTER TABLE od_15min ADD PRIMARY KEY (o,d,t);
+CREATE TABLE flows15m   AS (SELECT * FROM flows05m) WITH NO DATA;
+ALTER TABLE flows_15m ADD PRIMARY KEY (o,d,t);
 
-CREATE TABLE od_1hour   AS (SELECT * FROM od_05min) WITH NO DATA;
-ALTER TABLE od_1hour ADD PRIMARY KEY (o,d,t);
+CREATE TABLE flows1h   AS (SELECT * FROM flows05m) WITH NO DATA;
+ALTER TABLE flows1h ADD PRIMARY KEY (o,d,t);
 
-CREATE TABLE od_24hours AS (SELECT * FROM od_05min) WITH NO DATA;
-ALTER TABLE od_24hours ADD PRIMARY KEY (o,d,t);
+CREATE TABLE flows24h AS (SELECT * FROM flows05m) WITH NO DATA;
+ALTER TABLE flows24h ADD PRIMARY KEY (o,d,t);
 
-CREATE TABLE od_7days   AS (SELECT * FROM od_05min) WITH NO DATA;
-ALTER TABLE od_7days ADD PRIMARY KEY (o,d,t);
-
-CREATE TABLE od_total   AS (SELECT * FROM od_05min) WITH NO DATA;
-ALTER TABLE od_total ADD PRIMARY KEY (o,d,t);
+CREATE TABLE flows7d   AS (SELECT * FROM flows05m) WITH NO DATA;
+ALTER TABLE flows7d ADD PRIMARY KEY (o,d,t);
 
 -- -- Create hypertables:
 -- --   + default chunk_time_interval = 1 week
@@ -34,65 +31,11 @@ ALTER TABLE od_total ADD PRIMARY KEY (o,d,t);
 -- --   + Q1: what is the memory footprint of 1 week of data?
 -- --   + Q2: how much memory can my server allocate to the database?
 
-SELECT create_hypertable('od_05min'  , 't', chunk_time_interval => interval '1 month');
-SELECT create_hypertable('od_15min'  , 't', chunk_time_interval => interval '2 months');
-SELECT create_hypertable('od_1hour'  , 't', chunk_time_interval => interval '4 months');
-SELECT create_hypertable('od_24hours', 't', chunk_time_interval => interval '12 months');
-SELECT create_hypertable('od_7days'  , 't', chunk_time_interval => interval '12 months');
-
-
---- Aggregate tables
-
-CREATE TABLE agg_hour_weekday
-(
-  o INTEGER NOT NULL,
-  d INTEGER NOT NULL,
-  hour INTEGER NOT NULL,
-  weekday INTEGER NOT NULL,
-  sum_flow INTEGER,
-  median_flow NUMERIC,
-  sum_mean_avspeed NUMERIC,
-  median_mean_avspeed NUMERIC,
-  sum_sd_avspeed NUMERIC,
-  median_sd_avspeed NUMERIC,
-  sum_median_avspeed NUMERIC,
-  median_median_avspeed NUMERIC,
-  PRIMARY KEY(o,d,hour,weekday)
-);
-
-CREATE TABLE agg_05min_weekdaycat
-(
-  o INTEGER NOT NULL,
-  d INTEGER NOT NULL,
-  period TIME NOT NULL,
-  weekdaycat INTEGER NOT NULL,
-  sum_flow INTEGER,
-  median_flow NUMERIC,
-  sum_mean_avspeed NUMERIC,
-  median_mean_avspeed NUMERIC,
-  sum_sd_avspeed NUMERIC,
-  median_sd_avspeed NUMERIC,
-  sum_median_avspeed NUMERIC,
-  median_median_avspeed NUMERIC,
-  PRIMARY KEY(o,d,period,weekdaycat)
-);
-
-CREATE TABLE agg_15min_weekdaycat
-(
-  o INTEGER NOT NULL,
-  d INTEGER NOT NULL,
-  period TIME NOT NULL,
-  weekdaycat INTEGER NOT NULL,
-  sum_flow INTEGER,
-  median_flow NUMERIC,
-  sum_mean_avspeed NUMERIC,
-  median_mean_avspeed NUMERIC,
-  sum_sd_avspeed NUMERIC,
-  median_sd_avspeed NUMERIC,
-  sum_median_avspeed NUMERIC,
-  median_median_avspeed NUMERIC,
-  PRIMARY KEY(o,d,period,weekdaycat)
-);
+SELECT create_hypertable('flows05m', 't', chunk_time_interval => interval '1 month');
+SELECT create_hypertable('flows15m', 't', chunk_time_interval => interval '2 months');
+SELECT create_hypertable('flows1h' , 't', chunk_time_interval => interval '4 months');
+SELECT create_hypertable('flows24h', 't', chunk_time_interval => interval '12 months');
+SELECT create_hypertable('flows7d' , 't', chunk_time_interval => interval '12 months');
 
 --- Functions
 
